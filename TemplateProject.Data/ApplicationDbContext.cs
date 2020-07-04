860db8e0205240a7887cae2f1d976169a7159f2d
Reece Baird <reece@baddinosaur.co.uk>
2020-07-04 01:19:26 +0100

    Add project files.

---
+++ TemplateProject.Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace TemplateProject.Web.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            this.ChangeTracker.LazyLoadingEnabled = false;
+        }
+    }
+}
